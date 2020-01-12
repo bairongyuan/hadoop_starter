94fff0086fc7c904b23451dba76843dd127d32a4
bairongyuan <xiaoap009@163.com>
2020-01-12 23:43:03 +0800

    add use DIY combiner

---
+++ mapreduce_study3/src/main/java/com/kkb/mr_combiner/WordCountMain.java
@@ -0,0 +1,51 @@
+package com.kkb.mr_combiner;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCountMain extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = super.getConf();
+        Job job = Job.getInstance(conf, "combineDemo");
+
+        job.setJarByClass(WordCountMain.class);
+
+//        job.setInputFormatClass(CombineFileInputFormat.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        TextInputFormat.addInputPath(job, new Path("file:///d:\\360\\1.txt"));
+        job.setMapperClass(WordCountMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setOutputFormatClass(TextOutputFormat.class);
+        TextOutputFormat.setOutputPath(job, new Path("file:///d:\\360\\combiner_out1"));
+
+        job.setNumReduceTasks(3);
+
+        boolean b = job.waitForCompletion(true);
+        return b?0:1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.set("hello", "world");
+        int run = ToolRunner.run(configuration, new WordCountMain(), args);
+        System.exit(run);
+    }
+}
